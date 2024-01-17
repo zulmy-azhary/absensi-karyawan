f2df14926a15ddbdc6163f9e60622cbbbb19cd7e
zoel-a <zulmyazhary32@gmail.com>
2024-01-17 17:11:10 +0800

    Add custom hooks

---
+++ app/hooks/useToggle.ts
@@ -0,0 +1,11 @@
+import { type Dispatch, type SetStateAction, useCallback, useState } from "react";
+
+type Props = [boolean, () => void, Dispatch<SetStateAction<boolean>>];
+
+const useToggle = (defaultValue?: boolean): Props => {
+  const [isOpen, setOpen] = useState<boolean>(!!defaultValue);
+  const onToggle = useCallback(() => setOpen((value) => !value), []);
+  return [isOpen, onToggle, setOpen];
+};
+
+export default useToggle;
