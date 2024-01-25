1c25051cf4ecdafaeed1d9d9c274c18c5f4bdf5a
zoel-a <zulmyazhary32@gmail.com>
2024-01-25 18:38:08 +0800

    Update middleware

--- app/middlewares/auth.middleware.ts
+++ app/middlewares/auth.middleware.ts
@@ -1,16 +1,29 @@
 import { redirect } from "@remix-run/node";
-import { authenticator } from "~/services/auth.server";
+import { AuthorizationError } from "remix-auth";
+import { authenticator } from "~/actions/auth.server";
 
 export async function isAdmin(request: Request) {
   const user = await authenticator.isAuthenticated(request);
-  if (user?.role !== "Admin") {
-    throw redirect("/app/profile");
+  if (!user) {
+    throw new AuthorizationError("Forbidden user.");
   }
+
+  if (user.role !== "Admin") {
+    throw redirect("/app/profil");
+  }
+
+  return user;
 }
 
 export async function isKaryawan(request: Request) {
   const user = await authenticator.isAuthenticated(request);
-  if (user?.role !== "Karyawan") {
+  if (!user) {
+    throw new AuthorizationError("Forbidden user.");
+  }
+
+  if (user.role !== "Karyawan") {
     throw redirect("/app");
   }
+
+  return user;
 }
