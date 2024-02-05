705ce515f99bf447aabbf7766522bf3af4723f3d
zoel-a <zulmyazhary32@gmail.com>
2024-02-06 00:07:39 +0800

    fix: absenceState utility function

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,20 +1,54 @@
 import { type ClassValue, clsx } from "clsx";
 import { isWithinInterval, set } from "date-fns";
 import { twMerge } from "tailwind-merge";
+import { getAttendanceById } from "~/services/absence.server";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+type CurrentPositionType = google.maps.LatLngLiteral;
+type TargetPositionType = google.maps.LatLngLiteral;
+
+export function calculateDistance(
+  currentPosition: CurrentPositionType,
+  targetPosition: TargetPositionType
+): number {
+  // Konversi derajat ke radian
+  let lat1 = (currentPosition.lat * Math.PI) / 180;
+  let lng1 = (currentPosition.lng * Math.PI) / 180;
+  let lat2 = (targetPosition.lat * Math.PI) / 180;
+  let lng2 = (targetPosition.lng * Math.PI) / 180;
+
+  // Haversine Formula
+  let distanceLat = Math.abs(lat2 - lat1);
+  let distanceLng = Math.abs(lng2 - lng1);
+  let a =
+    Math.pow(Math.sin(distanceLat / 2), 2) +
+    Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(distanceLng / 2), 2);
+
+  let calculate = 2 * Math.asin(Math.sqrt(a));
+
+  // Radius bumi, 6371 KM
+  let radius = 6371;
+  let distanceKm = calculate * radius;
+
+  return Math.round(distanceKm * 1000);
+}
+
+
 type AbsenceState =
   | "Belum Bisa Absen Masuk"
   | "Lakukan Absen Masuk"
+  | "Sudah Absen Masuk"
   | "Absen Masuk Sudah Lewat"
   | "Belum Bisa Absen Keluar"
   | "Lakukan Absen Keluar"
+  | "Sudah Absen Keluar"
   | "Absen Keluar Sudah Lewat";
 
-export function absenceState(date: Date): AbsenceState | undefined {
+export async function absenceState(date: Date, absenceId?: string): Promise<AbsenceState | undefined> {
+  const attendanceToday = await getAttendanceById(absenceId ?? "");
   //* Jika waktu sekarang belum jam 08:00AM (00:01AM - 07:59AM), maka belum dapat melakukan absen masuk.
   const isAbsenceInNotReadyYet = isWithinInterval(date, {
     start: set(date, { hours: 0, minutes: 0 }),
@@ -30,6 +64,9 @@ export function absenceState(date: Date): AbsenceState | undefined {
     end: set(date, { hours: 8, minutes: 59 }),
   });
   if (isAbsenceIn) {
+    if (attendanceToday.find(attendance => attendance.status === "Masuk")) {
+      return "Sudah Absen Masuk";
+    }
     return "Lakukan Absen Masuk";
   }
 
@@ -57,6 +94,9 @@ export function absenceState(date: Date): AbsenceState | undefined {
     end: set(date, { hours: 21, minutes: 59 }),
   });
   if (isAbsenceOut) {
+    if (attendanceToday.find((attendance) => attendance.status === "Keluar")) {
+      return "Sudah Absen Keluar";
+    }
     return "Lakukan Absen Keluar";
   }
 
