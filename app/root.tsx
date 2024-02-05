e0c8e5ba1a7979f25f7432bba21b12fde9b30af2
zoel-a <zulmyazhary32@gmail.com>
2024-02-06 00:02:54 +0800

    feat: add fullcalendar's style head

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,7 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <style data-fullcalendar />
         <Links />
       </head>
       <body>
