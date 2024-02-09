371b2bde192f254b905eb89217f444872c2034d9
zoel-a <zulmyazhary32@gmail.com>
2024-02-09 21:33:11 +0800

    refactor: update ActionType for CREATE action

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,4 @@
 import type { Prisma } from "@prisma/client";
 
-export type ActionType = "DETAIL" | "UPDATE" | "DELETE";
+export type ActionType = "DETAIL" | "CREATE" | "UPDATE" | "DELETE";
 export type UserType = Omit<Prisma.UserGetPayload<{}>, "password">;
