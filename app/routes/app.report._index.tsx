f63ede824b9b585da5002f5f2577e28ea8c5125b
zoel-a <zulmyazhary32@gmail.com>
2024-02-19 22:10:34 +0800

    fix: report monitoring per month

--- app/routes/app.report._index.tsx
+++ app/routes/app.report._index.tsx
@@ -1,21 +1,77 @@
 import type { MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import {
+  addDays,
+  differenceInBusinessDays,
+  differenceInDays,
+  eachWeekendOfMonth,
+  getDaysInMonth,
+  getWeeksInMonth,
+} from "date-fns";
 import { Card } from "~/components/ui/card";
 import {
   Table,
   TableBody,
+  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "~/components/ui/table";
+import { dateParsed } from "~/lib/utils";
 import { getReport } from "~/services/report.server";
 
 export const loader = async () => {
-  return await getReport();
-  // try {
-  // } catch (error: unknown) {
-  //   return error as Error;
-  // }
+  const { users, absences } = await getReport();
+
+  const reports = users.map((user) => {
+    const getTotalDaysInMonth = getDaysInMonth(dateParsed(new Date()));
+    const totalHadir = absences.filter(
+      (absence) => absence.attendance.length === 2 && user.nik === absence.nik
+    ).length;
+
+    const absenceIzin = absences.filter(
+      (absence) => absence.submission?.status === "Izin" && user.nik === absence.nik
+    );
+
+    const absenceSakit = absences.filter(
+      (absence) => absence.submission?.status === "Sakit" && user.nik === absence.nik
+    );
+
+    // Get total days between startDate and endDate of izin except weekend
+    const sumOfIzin = absenceIzin.reduce(
+      (acc, curr) =>
+        acc +
+        differenceInBusinessDays(
+          addDays(curr.submission?.endDate!, 1),
+          curr.submission?.startDate!
+        ),
+      0
+    );
+
+    // Get total days between startDate and endDate of sakit except weekend
+    const sumOfSakit = absenceSakit.reduce(
+      (acc, curr) =>
+        acc +
+        differenceInBusinessDays(
+          addDays(curr.submission?.endDate!, 1),
+          curr.submission?.startDate!
+        ),
+      0
+    );
+
+    return {
+      nik: user.nik,
+      name: user.name,
+      hadir: totalHadir,
+      alpa:
+        getTotalDaysInMonth -
+        eachWeekendOfMonth(dateParsed(new Date())).length -
+        (totalHadir + sumOfIzin + sumOfSakit),
+      sakit: absenceSakit.length,
+      izin: absenceIzin.length,
+    };
+  });
+  return reports;
 };
 
 export const meta: MetaFunction = () => {
@@ -23,24 +79,35 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Report() {
-  const allAbsence = useLoaderData<typeof loader>();
-  console.log(allAbsence);
+  const reports = useLoaderData<typeof loader>();
 
   return (
     <div className="space-y-6">
-      <h2 className="text-lg font-medium">Laporan</h2>
+      <h2 className="text-lg font-medium">Laporan Bulanan</h2>
       <Card className="px-6 py-8">
         <Table className="mt-5 text-center border-y">
           <TableHeader className="bg-slate-100">
             <TableRow>
               <TableHead>Nama</TableHead>
               <TableHead>Nomor Induk Karyawan</TableHead>
-              <TableHead>Alpa</TableHead>
-              <TableHead>Sakit</TableHead>
+              <TableHead>Hadir</TableHead>
               <TableHead>Izin</TableHead>
+              <TableHead>Sakit</TableHead>
+              <TableHead>Alpa</TableHead>
             </TableRow>
           </TableHeader>
-          <TableBody></TableBody>
+          <TableBody>
+            {reports.map((report) => (
+              <TableRow key={report.nik}>
+                <TableCell>{report.name}</TableCell>
+                <TableCell>{report.nik}</TableCell>
+                <TableCell>{report.hadir}</TableCell>
+                <TableCell>{report.izin}</TableCell>
+                <TableCell>{report.sakit}</TableCell>
+                <TableCell>{report.alpa}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
         </Table>
       </Card>
     </div>
--- app/services/report.server.ts
+++ app/services/report.server.ts
@@ -1,17 +1,47 @@
-import { lastDayOfMonth, set } from "date-fns";
-import { db } from "~/lib/db"
+import { getDaysInMonth, lastDayOfMonth, set } from "date-fns";
+import { db } from "~/lib/db";
+import { getUsers } from "./user.server";
+import { dateParsed } from "~/lib/utils";
 
 export const getReport = async () => {
   const firstMonth = set(new Date(), { date: 1, hours: 0, minutes: 0, seconds: 0 });
   const lastMonth = set(lastDayOfMonth(new Date()), { hours: 23, minutes: 59, seconds: 59 });
 
-  return await db.absence.count({
-    where: {
-      nik: "111111111",
-      createdAt: {
-        gte: firstMonth,
-        lte: lastMonth
-      }
-    },
+  return await db.$transaction(async (tx) => {
+    const users = await getUsers();
+
+    const absences = await tx.absence.findMany({
+      where: {
+        OR: [
+          {
+            createdAt: {
+              gte: firstMonth,
+              lte: lastMonth,
+            },
+          },
+          {
+            createdAt: {
+              gte: firstMonth,
+              lte: lastMonth,
+            },
+            submission: {
+              isApproved: true,
+            },
+          },
+        ],
+      },
+      include: { attendance: true, submission: true },
+    });
+
+    const test = users.map(
+      (user) =>
+        absences
+          .filter((absence) => absence.attendance.length === 2)
+          .filter((absence) => user.nik === absence.nik).length
+    );
+
+    const getTotalDaysInMonth = getDaysInMonth(dateParsed(new Date()));
+
+    return { users, absences };
   });
-}
+};
